8a369ac24ca2c509660ebb9615ae34dba307abbb
Ojas Sinha <80882466+OSinA04@users.noreply.github.com>
2023-10-13 08:52:36 +0530

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+function displayFileName() {
+    const fileInput = document.getElementById("file");
+    const fileNameSpan = document.getElementById("file-name");
+  
+    if (fileInput.files.length > 0) {
+      fileNameSpan.textContent = fileInput.files[0].name;
+    } else {
+      fileNameSpan.textContent = "";
+    }
+  }
+  
+function captureImage() {
+fetch("/cam-hi.jpg")
+    .then((response) => {
+    if (response.status === 200) {
+        alert("Image captured successfully!");
+    } else {
+        alert("Failed to capture image. Check ESP32-CAM connection.");
+    }
+    })
+    .catch((error) => {
+    console.error("Error:", error);
+    });
+}
